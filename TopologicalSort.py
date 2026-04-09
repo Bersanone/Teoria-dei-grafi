6c12d5c7f8aacbedd9deeff982d27a1b150a18b4
Bersanone <andreabersani51@gmail.com>
2026-04-09 20:02:00 +0200

    topologicalsort commentato

--- TopologicalSort.py
+++ TopologicalSort.py
@@ -10,28 +10,39 @@
 
 class topologicalSortAdjacencyList:
 
+    #Inizializzazione della classe Edge
+
     class Edge:
-
-        from_node = 0
-        to = 0
-        weight = 0
-
+        #Init della classe Edge
 
         def __init__(self, from_node : int,to : int,weight : int):
 
             self.from_node = from_node
             self.to = to
+            #Peso dell'edge (il peso può rappresentare qualsiais cosa, ad esmepio la distanza)
             self.weight = weight
+
+
+    #Funzione di main
 
 
     def main(self):
 
+        #Impostiamo il numero di nodi a 7
+
         n : int = 7
+
+        #Istanziamo un grafo
 
         graph : dict[int, list[Edge]] = {}
 
-        for i in n:
+        #Aggiungiamo un array al grafo per ogni nodo
+
+        for i in range(n):
             graph[i] = []
+
+        
+        #Impostiamo gli archi come: Edge(from,to,weight)
 
 
         graph[0].append(self.Edge(0,1,3))
@@ -45,35 +56,17 @@ class topologicalSortAdjacencyList:
         graph[5].append(self.Edge(5,4,7))
 
 
+        #Calcoliamo la topological sort
+
+
         ordering : list[int] = self.topologicalsort(graph, n)
         print(str(ordering))
 
-
+        #Calcoliamo il percorso più breve
         dists : list[int] = self.dagShortestPath(graph,0,n)
 
         print(dists[4])
         print(dists[6])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     #Funzioni pubbliche
@@ -81,36 +74,56 @@ class topologicalSortAdjacencyList:
 
     def topologicalsort(self, graph : dict[int, list[Edge]], numNodes : int) -> list[int]:
 
-        self.ordering : list[int] = [0] * numNodes
-        self.visited : list[bool] = [False] * numNodes
+        #Creiamo una lista di ordering, inizializziamo tutto con 0 
 
+        ordering : list[int] = [0] * numNodes
+
+        #Creiamo una lista per segnalare i visitati, inizializziamo a false 
+        visited : list[bool] = [False] * numNodes
+        #Settiamo l'indice per la lista di ordering
 
         i : int = numNodes - 1
 
-        for at in numNodes:
-            if not self.visited[at]:
-                i = self.__dfs(i, at, self.visited, self.ordering, graph)
+        #Per ogni nodo non visitato eseguiamo la dfs e salviamo l'indice per il nodo successivo
 
-        return self.ordering
+        for at in range(numNodes):
+            if not visited[at]:
+                i = self.__dfs(i, at, visited, ordering, graph)
+
+        #Restituiamo la lista
+
+        return ordering
 
 
     #Metodo privato
 
     def __dfs(self,i : int, at : int, visited : list[bool], ordering : list[int], graph : dict[int, list[Edge]]) -> int:
 
-        self.visited[at] = True
-        
+        #Impostiamo il nodo corrrente come visitato
+
+        visited[at] = True
+
+
+
+
+
+        #Recuperiamo gli archi in uscita del node corrente
 
         edges: list[self.Edge] = graph[at]
 
 
-        if edges is not None:
-            for e in edges:
+        #Per ogni nodo adiacente non ancora visitato eseguiamo ricursivamente la dfs
+        for e in edges:
                 if not visited[e.to]:
                     i = self.__dfs(i, e.to, visited, ordering, graph)
 
 
+        #Inseriamo il nodo corrente nell'array partendo dal fondo (post-order)
+
+
         ordering[i] = at
+
+        #restituiamo l'indice decrementato per il prossimo nodo da inserire
         return i - 1
     
 
@@ -118,26 +131,48 @@ class topologicalSortAdjacencyList:
 
     def dagShortestPath(self, graph : dict[int, list[Edge]], start : int, numNodes : int) -> list[int]:
 
+        #Calcoliamo l'ordinamento topologico del grafo.
+
         topsort : list[int] = self.topologicalsort(graph, numNodes)
-        dist : list[int] = [0] * numNodes
+
+        #Inizializziamo l'array delle distanze a infinito. Questo garantisce che ogni nuova distanza calcolata sarà inizialmente minore del valore di default.
+        dist : list[int] = [float('inf')] * numNodes
+
+        #Impostiamo a 0 la distanza del nodo di partenza (start), poiché la distanza da un nodo a sé stesso è nulla.
 
         dist[start] = 0
 
-        for i in numNodes:
+        for i in range(numNodes):
+            #Otteniamo il nodo corrente seguendo rigorosamente l'ordine topologico.
             nodeIndex : int = topsort[i] 
 
-            if dist[nodeIndex] is None:
+            #Se il nodo corrente è irraggiungibile (distanza infinita), saltiamo l'iterazione poiché non può aggiornare i suoi vicini.
+
+            if dist[nodeIndex] == float('inf'):
                 continue
+
+            #Per ogni arco in uscita dal nodo corrente, calcoliamo la potenziale nuova distanza verso la destinazione.
 
             for edge in graph.get(nodeIndex, []):
                 newDist : int = dist[nodeIndex] + edge.weight
 
-                if dist[edge.to] is None or newDist < dist[edge.to]:
+                #Processo di 'Relaxation': se abbiamo trovato un percorso più breve, aggiorniamo la distanza minima verso il nodo di destinazione.
+
+                if newDist < dist[edge.to]:
 
                     dist[edge.to] = newDist
 
+        #restituiamo le distanze minime
+
         return dist
     
+
+
+
+
+p = topologicalSortAdjacencyList()
+
+p.main()
 
 
 
